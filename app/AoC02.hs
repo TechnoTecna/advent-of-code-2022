641e7c64371ba14db0f0f0c49d5f880bdd1d8d5c
Techno Tecna <techno.tecna@proton.me>
2022-12-03 23:05:48 +0100

    Added comments to AoC02

--- app/AoC02.hs
+++ app/AoC02.hs
@@ -13,12 +13,15 @@ data Inst = X | Y | Z
   deriving (Show, Eq)
 
 type Input = [(Move, Inst)]
+
 rawToInput :: String -> Input
 rawToInput =
-  map ( U.second (fromJust . flip lookup insts . head)
-        . U.first (fromJust . flip lookup mvs . head)
-        . splitAt 2 )
-      . lines
+  map (
+    U.second (fromJust . flip lookup insts . head) -- (Rock, "Y") -> (Rock, Y)
+    . U.first (fromJust . flip lookup mvs . head)  -- ("A ", "Y") -> (Rock, "Y")
+    . splitAt 2                                    -- "A Y" -> ("A ", "Y")
+  )       -- ["A Y", "B X", "C Z"] -> [(Rock, Y), (Paper, X), (Scissors, Z)]
+  . lines -- "A Y\nB X\nC Z" -> ["A Y", "B X", "C Z"]
   where mvs = [('A', Rock), ('B', Paper), ('C', Scissors)]
         insts = [('X', X), ('Y', Y), ('Z', Z)]
 
@@ -27,10 +30,18 @@ data Result = Win | Loss | Draw
   deriving (Eq, Show)
 
 moveOrder :: [Move]
+-- A circle that encode wich move beats wich   Rock <-- Paper
+--                                             |            ^
+--                                             +->Scissors--+
 moveOrder = [Rock, Paper, Scissors]
 
 result :: (Move, Move) -> Result
 result (mOp, mPl) =
+  -- rotate move order to make the player's move `mPl` first.
+  -- for example if `mPl = Paper` we have
+  -- `m0 = Paper , m1 = Scissors , m2 = Rock`
+  -- in every case m0 is the player's move, m1 beats the player and m2 loses to
+  -- the player.
   case U.rotate (fromJust $ elemIndex mPl moveOrder) moveOrder of
     [m0, m1, m2] | m2 == mOp -> Win
                  | m1 == mOp -> Loss
@@ -49,12 +60,19 @@ instToMove :: Inst -> Move
 instToMove = fromJust . flip lookup [(X, Rock), (Y, Paper), (Z, Scissors)]
 
 type Result1 = Int
+
 f1 :: Input -> Result1
-f1 = sum . map (score . U.second instToMove)
+f1 = sum -- [8, 1, 6] -> 15
+     . map (
+         score                 -- (Rock, Paper) -> 8
+         . U.second instToMove -- (Rock, Y) -> (Rock, Paper)
+       ) -- [(Rock, Y), (Paper, X), (Scissors, Z)] -> [8, 1, 6]
 
 -- Part2
 findMove :: Move -> Result -> Move
 findMove mOp res =
+  -- Same princilple as for `result`. We just pick a move instead of testing
+  -- for equality.
   let mvs = U.rotate (fromJust $ elemIndex mOp moveOrder) moveOrder in
   case (res, mvs) of
     (Win, [_, mPl, _])  -> mPl
@@ -69,7 +87,11 @@ score2 (mOp, res) = mvPnts (findMove mOp res) + resPnts res
 
 type Result2 = Int
 f2 :: Input -> Result2
-f2 = sum . map (score2 . U.second instToResult)
+f2 = sum -- [4, 1, 7] -> 12
+     . map (
+         score2                  -- (Rock, Draw) -> 4
+         . U.second instToResult -- (Rock, Y) -> (Rock, Draw)
+       ) -- [(Rock, Y), (Paper, X), (Scissors, Z)] -> [4, 1, 7]
 
 -- Main
 main :: IO()
