6ff026681a2b6b0eb1d8082b876b48dc91341b53
Techno Tecna <techno.tecna@proton.me>
2022-12-03 22:40:47 +0100

    Added comments to AoC01

--- app/AoC01.hs
+++ app/AoC01.hs
@@ -7,18 +7,30 @@ import Utils as U
 
 -- Input parsing
 type Input = [[Int]]
+
+-- | "1000\n2000\n3000\n\n4000\n" -> [[1000, 2000, 3000], [4000]]
 rawToInput :: String -> Input
-rawToInput = map (map read) . splitWhen  null . lines
+rawToInput =
+  map (map read)    -- [["1", "2", "3"], ["4"]] -> [[1, 2, 3], [4]]
+  . splitWhen  null -- ["1", "2", "3", "", "4"] -> [["1", "2", "3"], ["4"]]
+  . lines           -- "1\n2\n3\n\n4" -> ["1", "2", "3", "", "4"]
 
 -- Part1
 type Result1 = Int
+
 f1 :: Input -> Result1
-f1 = maximum . map sum
+f1 = maximum   -- [6, 4, 11, 24] -> 45
+     . map sum -- [[1, 2, 3], [4], [5, 6], [7, 8, 9]] -> [6, 4, 11, 24]
 
 -- Part2
 type Result2 = Int
+
 f2 :: Input -> Result2
-f2 = sum . take 3 . reverse . sort . map sum
+f2 = sum       -- [24, 11, 6] -> 41`
+     . take 3  -- [24, 11, 6, 4] -> [24, 11, 6]
+     . reverse -- [4, 6, 11, 24] -> [24, 11, 6, 4]
+     . sort    -- [6, 4, 11, 24] -> [4, 6, 11, 24]
+     . map sum -- [[1, 2, 3], [4], [5, 6], [7, 8, 9]] -> [6, 4, 11, 24]
 
 -- Main
 main :: IO()
